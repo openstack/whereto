68c3636d87b630f7bbc6c8b8efad5afd9e394fe3
Doug Hellmann <doug@doughellmann.com>
2017-08-23 12:13:31 -0400

    add parser
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ whereto/parser.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2010-2011 OpenStack Foundation
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import shlex
+
+
+def parse_rules(fd):
+    """Parse an open file containing redirect rules.
+
+    Generates a sequence of tuples containing line numbers and the
+    parsed lines.
+
+    Blank lines and lines starting with # are skipped.
+
+    :param fd: Open file handle or other data source supporting
+               iteration over lines, producing unicode strings.
+    :type fd: io.BufferedReader
+    """
+    for num, line in enumerate(fd, 1):
+        line = line.strip()
+        if not line:
+            continue
+        if line.startswith('#'):
+            continue
+        yield (num, shlex.split(line))
---
+++ whereto/tests/test_parser.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import io
+import textwrap
+
+from whereto import parser
+from whereto.tests import base
+
+
+class TestParseRules(base.TestCase):
+
+    def parse(self, text):
+        input = io.StringIO(textwrap.dedent(text))
+        return list(parser.parse_rules(input))
+
+    def test_skip_comments(self):
+        input = """
+        #
+        """
+        self.assertEqual(
+            [],
+            self.parse(input),
+        )
+
+    def test_skip_blank_lines(self):
+        input = """
+
+        """
+        self.assertEqual(
+            [],
+            self.parse(input),
+        )
+
+    def test_no_quotes(self):
+        input = """
+        redirect /path /new/path
+        """
+        self.assertEqual(
+            [(2, ['redirect', '/path', '/new/path'])],
+            self.parse(input),
+        )
+
+    def test_strip_quotes(self):
+        input = """
+        redirectmatch 301 "^/releases.*$" http://releases.openstack.org$1
+        """
+        self.assertEqual(
+            [(2, ['redirectmatch', '301', '^/releases.*$',
+                  'http://releases.openstack.org$1'])],
+            self.parse(input),
+        )
